5d98d8ced533456ee77f5f3c82ec84181e4c4a52
tianlixin <tianlixin@neucloud.cn>
2019-04-16 19:48:29 +0800

    delete str

--- test.js
+++ test.js
@@ -2,16 +2,4 @@ let str = 'hello world 2333'
 let str2 = 'hello git'
 let str3 = 'hello everyBody'
 
-let str4 = 'rebaseTest add str4'
-
-let str7 = '888'
-console.log(str,str2,str3)
-let str5 = '2333'
-
-let str6 = '4666'
-
-let str9 = 'heheheeheheh'
-
-let str10 = 'hiahiahiahaihaihhah'
-
 console.log(str,str2,str3)  
